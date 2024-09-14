f243a68c6771792fda3e9cbd739ece8c6cbf3d45
ARN <this.is.arn@yandex.ru>
2024-09-15 03:29:42 +0400

    minor fix const

--- backend/src/configuration/configuration.ts
+++ backend/src/configuration/configuration.ts
@@ -2,12 +2,13 @@ export default () => ({
   server: {
     port: parseInt(process.env.PORT, 10) || 3000,
   },
+  // переименовываем переменные окружения для возможности подключения докер-контейнера postgres
   database: {
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5433,
-    username: process.env.DATABASE_USERNAME || 'student',
-    password: process.env.DATABASE_PASSWORD || 'student',
-    name: process.env.DATABASE_NAME || 'kupipodariday',
+    host: process.env.POSTGRES_HOST || 'postgres',
+    port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+    username: process.env.POSTGRES_USER || 'student',
+    password: process.env.POSTGRES_PASSWORD || 'student',
+    name: process.env.POSTGRES_DB || 'kupipodariday',
   },
   jwt: {
     key: process.env.JWT_KEY || 'super-strong-secret',
